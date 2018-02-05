d870d9a42079e97bba35e1994f7221118700dba9
Frinkets <32484211+Frinkets@users.noreply.github.com>
2018-02-05 14:43:58 +0600

    Update Program.cs

--- HomeWork01252018/Program.cs
+++ HomeWork01252018/Program.cs
@@ -46,7 +46,9 @@ namespace HomeWork01252018
 
             foreach (var item in db.Areas)
             {
-                DirectoryInfo dr = new DirectoryInfo(@"C:\Users\Евгений\Desktop\AdoLesson4\AdoLesson4\bin\Debug\Areas\" + item.Name+"("+item.AreaId+")");
+                
+                DirectoryInfo dr = new
+                    DirectoryInfo(@"C:\Users\Евгений\Desktop\AdoLesson4\AdoLesson4\bin\Debug\Areas\" + item.Name+"("+item.AreaId+")");
                dr.Create();
                     XDocument xDoc = new XDocument(
                         new XElement("Area",
@@ -57,12 +59,132 @@ namespace HomeWork01252018
 
             }
 
-
-          
-
-
-
-
+         
+                
         }
+         public static void Task3()
+         {
+         
+            d.	Выгрузить из таблицы Timer, 
+            данные только для зон у которых есть IP адрес, 
+            при этом в XML файл должны войти следующие поля: 
+            UserId, Area Name (name из Талицы Area), DateStart
+                
+            var timer = 
+                from t in db.Timer
+                where t.IP != null & t.IP != ""
+                select t;
+             var timer2 = db.Timer
+                 .Where(t => t.IP != null & t.IP != "")
+                 .Join(db.Area, a => a.AreaID, t.AreaID,(a,t)=> new{ a,t });
+             
+        foreach (Timer item in timer)
+            {
+                XDocument xDoc = new XDocument(
+                    new XElement("Area",
+                    new XElement("AreaId", item.t.AreaID),
+                    new XElement("Name", item.a.Name),
+                    new XElement("Date", item.t.DateStart),
+                    new XElement("IP", item.a.IP)));
+                xDoc.Save(item.AreaId+ ".xml");
+            }
+         public static void Task4()
+         {
+            // e.	Выгрузить в XML файл, данные из таблицы Timer, у которых нет даты завершения работы DateFinish =null
+                var timer = 
+                from t in db.Timer
+                where t.DateFinish == null 
+                select t;
+             
+                      
+        foreach (Timer item in timer)
+            {
+                XDocument xDoc = new XDocument(
+                    new XElement("Area",
+                    new XElement("AreaId", item.t.AreaID),
+                    new XElement("Name", item.a.Name),
+                    new XElement("Date", item.t.DateStart),
+                    new XElement("IP", item.a.IP)));
+                xDoc.Save(item.AreaId+ ".xml");
+            }
+             
+         } 
+             
+              public static void Task4()
+         {
+                 // f.	Выгрузить весь список выполненных работ из таблицы Timer
+                   var timer = 
+                from t in db.Timer
+                where t.DateFinish != null
+                select t;
+                  
+                   foreach (Timer item in timer)
+            {
+                XDocument xDoc = new XDocument(
+                    new XElement("Area",
+                    new XElement("AreaId", item.t.AreaID),
+                    new XElement("Name", item.a.Name),
+                    new XElement("Date", item.t.DateStart),
+                    new XElement("IP", item.a.IP)));
+                xDoc.Save(item.AreaId+ ".xml");
+            }
+              }
+             
+                    public static void Task4()
+         {
+            // g.	Выгрузить данные с таблицы Area в переменную,
+           //  на основе данных в этой переменной создать XML файл имеющим Xmlns = «area», 
+           //  а также namespace - http://logbook.itstep.org/
+                        
+                 XNameSpace s = new  XNameSpace("http://logbook.itstep.org/");
+                        
+                           foreach (Area item in db.Area)
+            {
+                    XDocument xDoc = new XDocument(s,
+                        new XElement("Area",
+                        new XElement("AreaId", item.t.AreaID),
+                        new XElement("Name", item.a.Name),
+                        new XElement("Date", item.t.DateStart),
+                        new XElement("IP", item.a.IP)));
+                    xDoc.Save(item.AreaId+ ".xml");
+            }
+                        
+                public static void Task7()
+               {
+                     //a.	Вывести на экран поля UserId, AreaId, DocumentId из XML файла пункта F   
+                     XDocument xdoc = new XDocument();
+                    xdoc.Load("//myfile.xml");
+                    
+       foreach (var item in xdoc)
+            {
+              cw(item.element("UserID")) ;     
+             cw(item.element("AreaId")) ;  
+             cw(item.element("DocumentId")) ;  
+             
+            }
+                    
+                }
+                    public static void Task7()
+               {     
+                      //  b.	Выгрузить все данные из XML пункта F,
+                      //  заменить при этом в XML файла DateFinish =текущая дата 
+                       //   и сохранить данный XML файл с наименованием – 
+                       //  «TimeChangeToday_10.27.2017»
+                            
+                            
+                        
+                     XDocument xdoc = new XDocument();
+                    xdoc.Load("//myfile.xml");
+                      
+                        var rt = xdoc.First();
+                        rt.element("DateFinish") == Date.Now;
+                        xdoc.Save(rt);
+                        
+                        
+                            
+                    }
+                        
+                    }
+         }
     }
 }
